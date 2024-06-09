a602b40c79d4bb037fba4e1d901469483b64c3dd
vekemans <alexander.vekemans@gmail.com>
2024-06-09 19:29:58 +0200

    update

--- radon.c
+++ radon.c
@@ -15,8 +15,7 @@ static float f(int x, int y) {
 #define ht 1080u
 #define ch 4
 #define si 412 // sum iterations
-#define tau 6.2831853f
-#define rot 0.0175f // ~1 deg
+#define rot 0.5*0.0175f // ~1 deg
 
 #include <string.h>
 int main() {
@@ -33,31 +32,18 @@ int main() {
 	// then R(f(s,u)) = L[begin,end] f(s,u) du where L is a line integral
 	// [begin,end] should be large enough to cover the extent of the object
 
-	int const du = 48, ds = 48;
 	float z = 0;
 	do {
-		float tn = -tanf(z * 0.5), sn = sinf(z);
-		for (int x = wt-1; x > 0; x--) {
-			for (int y = ht-1; y > 0; y--) {
+		float tn = -tanf(z * 0.5), sn = sinf(z), sum = 0;
+		for (int x = wt-400; x > 400; x--) {
+			for (int y = ht-200; y > 200; y--) {
 				// rotation by shearing thrice
 				// cohost.org/tomforsyth/post/891823-rotation-with-three
-				int s = (x-960) * (1+tn*sn) + (y-540) * sn;
-				int u = (x-960) * (2*tn+tn*tn*sn) + (y-540) * (1+sn*tn);
-				float sum = 0;
-				// FIXME evaluate grid covering extent of object
-				// ie. grid is a fixed chunk of the screen
-				// which means we need to rotate the grid rather than the object/screen (?)
-				for (int i = 0; i < si; i += ds) {
-					for (int j = 0; j < si; j += du) {
-						sum += f(s-i,u-j);
-					}
-					//int q = 120*z; buf[(q+wt*i)*4+2] += 255u * sum; // sinogram
-				}
-				sum *= 1.0f / si; // [0,1]
-				//int q = 120*z; buf[(q+wt*y)*4+2] += 255u * sum; // sinogram
-				buf[(x+wt*y)*4+2] += 255u * sum; // disc
-				//buf[(x+wt*y)*4+1] = 255u * f(s,u); // rotating object
-				//buf[(x+wt*y)*4+0] = 255u * f(x-960,y-540); // centered object
+				int s = (x-960) * (1+tn*sn) + (y-540) * sn, u = (x-960) * (2*tn+tn*tn*sn) + (y-540) * (1+sn*tn);
+				//int s = x-960, u = y-540;
+				sum = 0.005f * (sum + f(s,u));
+				int q = 120*z; buf[(q+wt*y)*4+2] += 255u * sum; // sinogram
+				buf[(x+wt*y)*4+1] = 255u * f(s,u); // object
 			}
 		}
 		if (pwrite(fb, buf, wt*ht*ch, 0) < 0) {
